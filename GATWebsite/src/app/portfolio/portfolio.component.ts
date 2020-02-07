373f219f591117fafd2de4ee448be3555cccf20f
Hemant <varun.glory369@gmail.com>
2020-02-07 13:12:13 +0530

    Portfolio

--- GATWebsite/src/app/portfolio/portfolio.component.ts
+++ GATWebsite/src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+declare var jQuery: any;
+declare var AOS: any;
 
 @Component({
   selector: 'app-portfolio',
@@ -9,10 +11,36 @@ import { ActivatedRoute } from '@angular/router';
 export class PortfolioComponent implements OnInit {
 
   id;
-  constructor(private activateRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.id = this.activateRoute.snapshot.paramMap.get('id');
+  slideIndex = 1;
+  constructor(private activateRoute: ActivatedRoute) {
   }
 
+  ngAfterViewInit() {
+    this.showDivs(this.slideIndex);
+  }
+  ngOnInit() {
+    this.id = this.activateRoute.snapshot.paramMap.get('id');
+    (function ($) {
+      $(document).ready(function () {
+        AOS.init();
+      });
+    })(jQuery);
+  }
+  plusDivs(n) {
+    this.showDivs(this.slideIndex += n);
+  }
+  showDivs(n) {
+    var i;
+    var x: any = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+      this.slideIndex = 1
+    }
+    if (n < 1) {
+      this.slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[this.slideIndex - 1].style.display = "block";
+  }
 }
