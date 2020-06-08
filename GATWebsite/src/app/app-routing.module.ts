35754ab097a50e7287605abd8b19ffdfc998b14b
Hemant <varun.glory369@gmail.com>
2020-06-08 18:53:29 +0530

    Navigation

--- GATWebsite/src/app/app-routing.module.ts
+++ GATWebsite/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './dashboard/dashboard.module#DashboardModule' },
@@ -8,9 +8,13 @@ const routes: Routes = [
   { path: 'portfolio', loadChildren: './portfolio/portfolio.module#PortfolioModule' },
   { path: '**', redirectTo: '' },
 ];
-
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  // ...any other options you'd like to use
+};
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
